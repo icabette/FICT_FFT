1e9b372bf3dc5d82ee2cbfc1e11f867e5fe2e92f
icabette <icabette@gmail.com>
2022-06-29 18:09:36 +0900

    implemeted done.

--- fftMain.py
+++ fftMain.py
@@ -1,2 +1,22 @@
 ## 입력 받은 데이터를 FFT 진행 후, 원본 결과와 결과 값을 엑셀 형태로 출력하는 프로그램
 
+import random
+from scipy.fftpack import fft  
+import numpy as np  
+#x = np.array([4.0, 2.0, 1.0, -3.0, 1.5])  
+#y = fft(x)  
+#print(y)  
+
+
+def dataCreation():
+    data = []
+    for i in range (0,500):
+        data.append(float(format(random.uniform(0,6),".2f")))
+    return data
+
+def main():
+    input = dataCreation()
+    y = fft(input)
+    print(y)
+
+main()
