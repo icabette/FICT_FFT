a2d158d87feb3ff87b5e2d963478f036b2bf6c4d
icabette <icabette@gmail.com>
2022-07-01 00:17:44 +0900

    FFT 구현 완료

--- fftMain.py
+++ fftMain.py
@@ -2,18 +2,44 @@
 
 import random
 from scipy.fftpack import fft  
-#import numpy as np
+import openpyxl
 
 def dataCreation():
     data = []
-    for i in range (0,256):
+    for i in range (0,512):
         data.append(float(format(random.uniform(0,6),".2f")))
     return data
+
+def excelExport(base, result):
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = 'FFT'
+    sheet['A1'] = '원본데이터'
+    sheet['B1'] = 'FFT변환데이터'
+    
+    cellCount = 2
+    for i in base:
+        sheetNum = 'A' + str(cellCount)
+        sheet[sheetNum] = str(i)
+        cellCount = cellCount + 1
+    
+    cellCount = 2
+    lresult = result.tolist() # np.ndarray to list
+    for i in lresult:
+        sheetNum = 'B' + str(cellCount)
+        # 괄호 제거
+        sheet[sheetNum] = str(i).replace("(","").replace(")","")
+        cellCount = cellCount + 1
+    wb.save('FFT_Result.xlsx')
 
 def main():
     input = dataCreation()
     y = fft(input)
-    for i in y:
-        print (i)
+    excelExport(input, y)
 
+    """
+    for i in y:
+        print (str(i).replace("(","").replace(")",""))
+    """
+   
 main()
